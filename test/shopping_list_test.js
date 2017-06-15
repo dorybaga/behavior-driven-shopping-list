6244dc116c330a9ef697ea99f3bff45ca4f5b570
BK <bkam03@yahoo.com>
2017-06-14 21:13:36 -1000

    created shoppinglistitem class and module export line

--- test/shopping_list_test.js
+++ test/shopping_list_test.js
@@ -7,8 +7,18 @@ var ShoppingListItem = require('../js/shopping_list_item.js');
 
 describe('ShoppingListItem Class', function(){
   var shopList = ShoppingListItem;
+  var grocery;
+
+  beforeEach( function (){
+    grocery = new ShoppingListItem( 'onions' );
+  } );
 
   it('should be a Class', function(){
-    expect(shopList).to.be.a('Class');
+    expect(shopList).to.be.a('function');
   });
+
+  it( 'should have a property named "name"', function(){
+    expect( shopList.name ).to.equal( 'onions' );
+  } );
+
 });
