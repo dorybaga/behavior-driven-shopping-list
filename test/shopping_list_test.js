9e52e460439c4d71ac866f30873d1a9ef25da935
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-06-14 20:57:00 -1000

    0 tests passing

---
+++ test/shopping_list_test.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+
+var expect = chai.expect;
+var should = chai.should();
+
+var ShoppingListItem = require('../js/shopping_list_item.js');
+
+describe('ShoppingListItem Class', function(){
+  var shopList = ShoppingListItem;
+});
