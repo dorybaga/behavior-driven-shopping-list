4c66c78c05fa5a585d0df73baec031c40dfca0f0
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-06-14 21:00:35 -1000

    ShoppingListItem should be a class

--- test/shopping_list_test.js
+++ test/shopping_list_test.js
@@ -7,4 +7,8 @@ var ShoppingListItem = require('../js/shopping_list_item.js');
 
 describe('ShoppingListItem Class', function(){
   var shopList = ShoppingListItem;
+
+  it('should be a Class', function(){
+    expect(shopList).to.be.a('Class');
+  });
 });
