0aeaea18b5ae339a59dd9fc568774a83a954b4b7
BK <bkam03@yahoo.com>
2017-06-16 15:10:16 -1000

    modified removeItem method to throw error when item passed is not in items.  deleted on accident, so may appear as different file.

--- js/shopping_list.js
+++ js/shopping_list.js
@@ -8,7 +8,7 @@ class ShoppingList {
 
   addItem(item){
     if( item instanceof ShoppingListItem ){
-      console.log(`${item} is item`);
+      //console.log(`${item} is item`);
       this.items.push(item);
     } else {
       throw new Error('Error');
@@ -16,16 +16,20 @@ class ShoppingList {
   }
 
   removeItem(item){
+    var isItemInItems = false;
     if (item === undefined){
       this.items.pop();
     } else {
       for(var i = 0; i < this.items.length; i++){
         if (this.items[i].name === item){
           this.items.splice(i, 1);
+          isItemInItems = true;
         }
+      }
+      if(isItemInItems === false){
+        throw new Error('Error');
+
       }
     }
   }
 }
-
-
